eaf46cf462d01542770e81ee3969aba132c92af3
DallinSanti <89087175+DallinSanti@users.noreply.github.com>
2022-04-25 09:05:03 -0600

    Added my controller, frame, and panel tests

---
+++ src/calendar/tests/ControllerTest.java
@@ -0,0 +1,40 @@
+package calendar.tests;
+
+public class ControllerTest 
+{
+	import static org.junit.jupiter.api.Assertions.*;
+
+	import org.junit.jupiter.api.AfterEach;
+	import org.junit.jupiter.api.BeforeEach;
+	import org.junit.jupiter.api.Test;
+
+	class UpdateControllerTest
+	{
+		private Controller testedController;
+
+		@BeforeEach
+		void setUp() throws Exception
+		{
+			this.testedController = new Controller();
+		}
+
+		@AfterEach
+		void tearDown() throws Exception
+		{
+			this.testedController  = null;
+		}
+
+		@Test
+		void testBuildPokedexText()
+		{
+			assertTrue(testedController != null, "Can't be null");
+		}
+
+		@Test
+		void testGetPokemonData()
+		{
+			assertEquals("Description", testedController.toString(), "Words match");
+		}
+
+	}
+}
