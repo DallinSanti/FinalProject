680f21db2eecdb7e7c00c853543150ae541c43e1
DallinSanti <89087175+DallinSanti@users.noreply.github.com>
2022-04-25 09:22:34 -0600

    Updated Controller with CalendarFrame

--- src/calendar/controller/Controller.java
+++ src/calendar/controller/Controller.java
@@ -1,9 +1,27 @@
 package calendar.controller;
 
+import javax.swing.JOptionPane;
+
+import calendar.view.CalendarFrame;
+import calendar.view.CalendarFrame;
+
 public class Controller
 {
+private CalendarFrame frame;
+	
+	public Controller()
+	{
+		frame = new CalendarFrame(this);
+	}
+	
 	public void start()
 	{
-		
+		JOptionPane.showMessageDialog(frame,  "Welcome to Java Calendar!");
+	}
+	
+	public void handleError(Exception error)
+	{
+		JOptionPane.showMessageDialog(frame,  error.getMessage(), "Calendar Error!",
+				JOptionPane.ERROR_MESSAGE);
 	}
 }
