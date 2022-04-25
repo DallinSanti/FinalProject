2a16fcd7cda8f2d5811dc65d649d69dbedf2c770
DallinSanti <89087175+DallinSanti@users.noreply.github.com>
2022-04-25 09:43:27 -0600

    Deleted calendarPanel
    By deleting calendar panel we also had to delete the content inside setupLayout. But now the program runs

--- src/calendar/view/CalendarPanel.java
+++ src/calendar/view/CalendarPanel.java
@@ -15,7 +15,7 @@ import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import java.awt.GridLayout;
 
-
+import javax.swing.*;
 
 public class CalendarPanel extends JPanel
 {
@@ -24,7 +24,6 @@ public class CalendarPanel extends JPanel
 	private JButton previousButton;
 	private JButton nextButton;
 	private SpringLayout layout;
-	private CalendarPanel calendar;
 	private JButton saveButton;
 
 
@@ -36,7 +35,6 @@ public class CalendarPanel extends JPanel
 		this.layout = new SpringLayout();
 		this.menuPanel = new JPanel(new GridLayout(0,1));
 	
-		this.calendar = new CalendarPanel(controller);
 	
 			setupPanel();
 			setupListeners();
@@ -47,7 +45,7 @@ public class CalendarPanel extends JPanel
 	{
 		this.setLayout(layout);
 		this.setBackground(Color.DARK_GRAY);;
-		this.add(calendar);
+		
 		this.add(menuPanel);
 		menuPanel.add(saveButton);
 	}
@@ -59,14 +57,7 @@ public class CalendarPanel extends JPanel
 
 	private void setupLayout()
 	{
-		layout.putConstraint(SpringLayout.EAST, menuPanel, -25, SpringLayout.EAST, this);
-		layout.putConstraint(SpringLayout.NORTH, menuPanel, 0, SpringLayout.NORTH, calendar);
-		layout.putConstraint(SpringLayout.WEST, menuPanel, 10, SpringLayout.EAST, calendar);
-		layout.putConstraint(SpringLayout.SOUTH, menuPanel, 0, SpringLayout.SOUTH, calendar);
-		layout.putConstraint(SpringLayout.NORTH, calendar, 25, SpringLayout.NORTH, this);
-		layout.putConstraint(SpringLayout.WEST, calendar, 25, SpringLayout.WEST, this);
-		layout.putConstraint(SpringLayout.SOUTH, calendar, -25, SpringLayout.SOUTH, this);
-		layout.putConstraint(SpringLayout.EAST, calendar, -125, SpringLayout.EAST, this);
+		
 	}
 
 }
