e00762dd5439a72da081dae1b532b82249216db8
DallinSanti <89087175+DallinSanti@users.noreply.github.com>
2022-04-27 08:14:30 -0600

    Made getDate and getTime from a private class to a public class and I added a getDate message dialog to Controller.java

--- src/calendar/controller/Controller.java
+++ src/calendar/controller/Controller.java
@@ -3,20 +3,23 @@ package calendar.controller;
 import javax.swing.JOptionPane;
 
 import calendar.view.CalendarFrame;
-import calendar.view.CalendarFrame;
+import calendar.model.Calendar;
 
 public class Controller
 {
 private CalendarFrame frame;
+private Calendar calendar;
 	
 	public Controller()
 	{
+		this.calendar = new Calendar();
 		frame = new CalendarFrame(this);
 	}
 	
 	public void start()
 	{
-		JOptionPane.showMessageDialog(frame,  "Welcome to Java Calendar!");
+		String date = calendar.getDate();
+		JOptionPane.showMessageDialog(frame,  "Welcome to Java Calendar! \n" + date);
 	}
 	
 	public void handleError(Exception error)
--- src/calendar/model/Calendar.java
+++ src/calendar/model/Calendar.java
@@ -4,4 +4,41 @@ import java.time.LocalDateTime;
 
 public class Calendar 
 {
+	
+	public String getDate()
+	{
+		String date = "The date is ";
+
+		LocalDateTime currentDate = LocalDateTime.now();
+
+		String day = currentDate.getDayOfWeek().toString();
+		
+		date += day + ", the ";
+
+		day = currentDate.getDayOfMonth() + "";
+
+		date += day;
+
+		String month = " of " + currentDate.getMonth().toString();
+
+		date += month;
+
+		date += ", " + currentDate.getYear() + "\n";
+
+			return date;
+	}
+
+
+	private String getTime()
+	{	
+		String time = "The time is ";
+	
+		LocalDateTime currentTime = LocalDateTime.now();
+	
+		time += currentTime.getHour() + ":";
+		time += currentTime.getMinute() + "\n";
+	
+
+			return time;
+	}
 }
