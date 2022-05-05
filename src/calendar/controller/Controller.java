ea8ebafc0afdaade6739795da95e9b7a06839b48
DallinSanti <89087175+DallinSanti@users.noreply.github.com>
2022-05-05 09:47:54 -0600

    Added JavaDoc for each file

--- src/calendar/controller/Controller.java
+++ src/calendar/controller/Controller.java
@@ -6,6 +6,10 @@ import calendar.view.CalendarFrame;
 import calendar.view.DaysPanel;
 import calendar.model.Calendar;
 
+/**
+ * @author dsan1423
+ * This will start the welcome panel and show the date & time
+ */
 public class Controller
 {
 private CalendarFrame frame;
@@ -17,6 +21,9 @@ private Calendar calendar;
 		frame = new CalendarFrame(this);
 	}
 	
+	/**
+	 * Displays welcome message
+	 */
 	public void start()
 	{
 		String date = calendar.getDate();
@@ -25,7 +32,10 @@ private Calendar calendar;
 				+ time + "Click the ''Calendar'' button to continue to the calendar!");
 	}
 	
-	
+	/**
+	 * Handles errors
+	 * @param error
+	 */
 	public void handleError(Exception error)
 	{
 		JOptionPane.showMessageDialog(frame,  error.getMessage(), "Calendar Error!",
