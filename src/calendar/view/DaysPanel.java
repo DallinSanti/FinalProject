6a3863ce9221d74715b1ed15da49fef8f7849ce3
DallinSanti <89087175+DallinSanti@users.noreply.github.com>
2022-05-11 08:46:33 -0600

    lower case g to upper case G on "Gregorian calendar"

--- src/calendar/view/DaysPanel.java
+++ src/calendar/view/DaysPanel.java
@@ -134,7 +134,7 @@ public class DaysPanel extends JPanel
 	System.out.println();
 	
 	/**
-	 * Below is the equation to get the start of the month using a gregorian calendar
+	 * Below is the equation to get the start of the month using a Gregorian calendar
 	 */
 	int year = yearNum - 1;
 	int beginningDay = (1 + monthOffset[monthNum - 1] + 5*(year % 4) + 4*(year % 100) + 6*(year % 400)) % 7;
